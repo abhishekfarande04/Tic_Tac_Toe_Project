a187513e0b15285c9fca8bf5257cae9bb5aaf7db
Abhishek Farande <farandeabhishek04@gmail.com>
2024-03-26 01:35:19 +0530

    Implement game logic and UI enhancements for Tic Tac Toe
    - Initialized game with player X starting and a blank game grid
    - Added event listeners for box clicks to update the game state and UI
    - Implemented turn swapping logic to alternate between players
    - Enhanced game over detection and UI updates for game status
    - Updated styles in CSS for improved visual appeal and user interaction

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const boxes=document.querySelectorAll(".box");
 
-const gameInfo=document.querySelector(".game-Info");
+const gameInfo=document.querySelector(".game-info");
 
 const newGameBtn=document.querySelector(".btn");
 
 let currentPlayer;
 let gameGrid;
 
-const winningPositions= {
+const winningPositions= [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -16,23 +16,71 @@ const winningPositions= {
     [2,5,8],
     [0,4,8],
     [2,4,6],
-};
+];
 
 
 // lets create function yto initalise the game 
 function initGame() {
     currentPlayer="X";
-    gameGrid=["","","","","","","","","",];
+    gameGrid=["","","","","","","","","",]; //initially all boxes are blank
     newGameBtn.classList.remove(".active");
     gameInfo.innerText=`Current Player - ${currentPlayer}`;
+    // but we need to update the Grid in the UI also
+    boxes.forEach((box,index) => {
+        box.innerText ="";
+        boxes[index].style.pointerEvents="all";
+    })
 }
 
 initGame();
-// here we takes Boxes (list of all Boxes ) and insert an Event listener to get to know about which index box is clicked 
 
+// make HandleClick named Function contains following functionalities :
+// 1. it should update the player mark (X or O ) in UI 
+// 2. it should make marked Box unclickable 
+// 3.after Each turn function should swap the Mark of the player (turn of player )
+// 4. it should upadte the Game in Gamegrid also 
+// 5 .
+
+function swapturn() {
+    if(currentPlayer === "X") {
+        currentPlayer === "O";
+    } else {
+        currentPlayer === "X";
+    }
+    // UI updated
+    gameInfo.innerText=`Current Player - ${currentPlayer}`;
+}
+
+
+function handleClick(index) {
+    if (gameGrid[index] === "") {
+        boxes[index].innerText=currentPlayer; // updated in UI 
+        gameGrid[index]=currentPlayer; // upadted in Game Grid 
+
+        swapturn();
+
+        checkGameOver();
+    }
+}
+
+function checkGameOver() {
+    let answer="";
+    
+    
+
+
+
+
+
+    newGameBtn.classList.add(".active");
+}
+
+// here we takes Boxes (list of all Boxes ) and insert an Event listener to get to know about which index box is clicked 
 boxes.forEach((box,index) => {
     box.addEventListener("click", ()=> {
         handleClick(index);
+        console.log("Clicked");
     })
 })
 
+newGameBtn.addEventListener("click",initGame());
